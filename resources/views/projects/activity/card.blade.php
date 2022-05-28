512bc0ee37df48f39fa41efb0b2951464f510ae5
Drenyl <drenyltobi@gmail.com>
2022-05-28 16:44:29 +0800

    added light and dark theme to project

--- resources/views/projects/activity/card.blade.php
+++ resources/views/projects/activity/card.blade.php
@@ -2,7 +2,7 @@
     <ul class="text-xs">
         @foreach($project->activity as $activity)
 
-            <li class="{{ $loop->last ? '' : 'mb-1' }}">
+            <li class="text-default {{ $loop->last ? '' : 'mb-1' }}">
                 
                 @include("projects.activity.$activity->description")
                 <span class="text-gray-400">{{ $activity->created_at->diffForHumans() }}</span>
--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,10 +1,10 @@
 <div class="card flex flex-col" style="height: auto">
 
-    <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-900 pl-4 mb-3">
+    <h3 class="text-default font-normal text-xl py-4 -ml-5 border-l-4 border-blue-900 pl-4 mb-3">
         <a href="{{ $project->path() }}">{{ $project->title }}</a>
     </h3>
 
-    <div class="text-gray-400 mb-4 flex-1">{{ \Illuminate\Support\Str::limit($project->description, 100) }}</div>
+    <div class="text-default mb-4 flex-1">{{ \Illuminate\Support\Str::limit($project->description, 100) }}</div>
 
 
     @can('manage', $project)
